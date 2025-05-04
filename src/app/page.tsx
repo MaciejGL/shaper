9c3cd7304e9772a2d46a5f5f8e8f4fd669e8d5b3
Maciej Glowacki <m.glowacki01@gmail.com>
2025-05-04 23:21:01 +0200

    Refactor Home component to replace Main wrapper with a semantic <main> element, enhancing accessibility and improving code clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Main } from '@/components/main'
 import { ButtonLink } from '@/components/ui/button-link'
 
 export default function Home() {
   return (
-    <Main>
+    <main className="min-h-screen grid grid-rows-1 w-full bg-zinc-100">
       <div className="flex flex-col items-center gap-8 justify-center h-screen">
         <ButtonLink href="/login">Login</ButtonLink>
       </div>
-    </Main>
+    </main>
   )
 }
