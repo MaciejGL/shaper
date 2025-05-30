11dac8d6d1ee14b9208bb11fe6d0ed1a79483af7
Maciej Glowacki <m.glowacki01@gmail.com>
2025-05-30 22:38:35 +0200

    wip

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,4 +1,5 @@
 import { createYoga } from 'graphql-yoga'
+import { NextRequest } from 'next/server'
 
 import { getCurrentUser } from '@/lib/getUser'
 
@@ -17,34 +18,16 @@ export const config = {
   },
 }
 
-const yoga = createYoga({
-  graphqlEndpoint: '/api/graphql',
-  schema: schema,
-  logging: 'debug',
-  async context() {
-    const userSession = await getCurrentUser()
-    return {
-      user: userSession,
-    }
-  },
-  cors: {
-    origin: '*',
-    credentials: true,
-    allowedHeaders: [
-      'X-Custom-Header',
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'X-Requested-With',
-    ],
-    methods: ['POST', 'GET', 'OPTIONS'],
-  },
-})
-
-// export default createYoga<{
-//   req: NextApiRequest
-//   res: NextApiResponse
-// }>({
+// const yoga = createYoga({
+//   graphqlEndpoint: '/api/graphql',
+//   schema: schema,
+//   logging: 'debug',
+//   async context() {
+//     const userSession = await getCurrentUser()
+//     return {
+//       user: userSession,
+//     }
+//   },
 //   cors: {
 //     origin: '*',
 //     credentials: true,
@@ -57,27 +40,38 @@ const yoga = createYoga({
 //     ],
 //     methods: ['POST', 'GET', 'OPTIONS'],
 //   },
-//   schema,
-//   logging: 'debug',
-//   async context() {
-//     const userSession = await getCurrentUser()
-//     return {
-//       user: userSession,
-//     }
-//   },
-//   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-//   graphqlEndpoint: '/api/graphql',
 // })
 
-// Handler wrapper for Next.js API routes
-export async function GET(request: Request) {
-  return yoga.handleRequest(request, {})
-}
+const yoga = createYoga<{
+  req: NextRequest
+}>({
+  schema,
+  logging: 'debug',
+  graphqlEndpoint: '/api/graphql',
+  cors: {
+    origin: ['https://fit-space.app', 'https://www.fit-space.app'],
+    credentials: true,
+  },
+  async context() {
+    const userSession = await getCurrentUser()
+    return {
+      user: userSession,
+    }
+  },
+  fetchAPI: { Request, Response, Headers },
+})
 
-export async function POST(request: Request) {
-  return yoga.handleRequest(request, {})
-}
+export { yoga as GET, yoga as POST }
 
-export async function OPTIONS(request: Request) {
-  return yoga.handleRequest(request, {})
-}
+// // Handler wrapper for Next.js API routes
+// export async function GET(request: Request) {
+//   return yoga.handleRequest(request, {})
+// }
+
+// export async function POST(request: Request) {
+//   return yoga.handleRequest(request, {})
+// }
+
+// export async function OPTIONS(request: Request) {
+//   return yoga.handleRequest(request, {})
+// }
