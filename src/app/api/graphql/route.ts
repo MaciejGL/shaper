534684b8802ef61720fce26e38b37cb7b2e34ea0
Maciej Glowacki <m.glowacki01@gmail.com>
2025-05-30 22:32:32 +0200

    wip

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -10,6 +10,13 @@ export type GraphQLContext = {
 
 const schema = await createSchema()
 
+export const config = {
+  api: {
+    // Disable body parsing (required for file uploads)
+    bodyParser: false,
+  },
+}
+
 const yoga = createYoga({
   graphqlEndpoint: '/api/graphql',
   schema: schema,
@@ -20,12 +27,57 @@ const yoga = createYoga({
       user: userSession,
     }
   },
+  cors: {
+    origin: '*',
+    credentials: true,
+    allowedHeaders: [
+      'X-Custom-Header',
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    methods: ['POST', 'GET', 'OPTIONS'],
+  },
 })
+
+// export default createYoga<{
+//   req: NextApiRequest
+//   res: NextApiResponse
+// }>({
+//   cors: {
+//     origin: '*',
+//     credentials: true,
+//     allowedHeaders: [
+//       'X-Custom-Header',
+//       'Content-Type',
+//       'Authorization',
+//       'Accept',
+//       'X-Requested-With',
+//     ],
+//     methods: ['POST', 'GET', 'OPTIONS'],
+//   },
+//   schema,
+//   logging: 'debug',
+//   async context() {
+//     const userSession = await getCurrentUser()
+//     return {
+//       user: userSession,
+//     }
+//   },
+//   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
+//   graphqlEndpoint: '/api/graphql',
+// })
+
 // Handler wrapper for Next.js API routes
 export async function GET(request: Request) {
   return yoga.handleRequest(request, {})
 }
 
 export async function POST(request: Request) {
+  return yoga.handleRequest(request, {})
+}
+
+export async function OPTIONS(request: Request) {
   return yoga.handleRequest(request, {})
 }
