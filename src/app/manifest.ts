9b46968482252020085c643a481632ba9749da09
Maciej Glowacki <m.glowacki01@gmail.com>
2025-06-15 23:36:35 +0200

    feat: add `purpose` attribute to favicon entries in `manifest.ts`
    * Updated `android-chrome-192x192.png` and `android-chrome-512x512.png` to include `purpose: 'maskable'`

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,11 +16,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/favicons/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/favicons/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
