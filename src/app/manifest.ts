50b635eb5eb361fc08efa7bb37277f6143a0b805
Maciej Glowacki <m.glowacki01@gmail.com>
2025-06-17 16:27:02 +0200

    feat: update favicon purpose in `manifest.ts`
    * Change favicon purpose from `maskable` to `any` for better compatibility

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,13 +16,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/favicons/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
       {
         src: '/favicons/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
     ],
   }
