6b355d177632b2c33bcba27ac4e9bc82c2c9c023
Maciej Glowacki <m.glowacki01@gmail.com>
2025-09-18 20:09:08 +0200

    google auth db

---
+++ prisma/migrations/20250918180604_add_google_oauth_fields/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[googleId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "googleAccessToken" TEXT,
+ADD COLUMN     "googleId" TEXT,
+ADD COLUMN     "googleRefreshToken" TEXT,
+ADD COLUMN     "locale" TEXT,
+ADD COLUMN     "timezone" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_googleId_key" ON "public"."User"("googleId");
+
+-- CreateIndex
+CREATE INDEX "User_googleId_idx" ON "public"."User"("googleId");
